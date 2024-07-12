240746c00cd071dbffd1dd35abd7a01031da9c56
KevJSDevelopment <Kshhn12@gmail.com>
2024-07-12 19:23:36 -0400

    step 1 table printed out

--- compression_tool/Program.cs
+++ compression_tool/Program.cs
@@ -13,11 +13,21 @@
             using StreamReader streamReader = new StreamReader(filePath);
 
             string content = streamReader.ReadToEnd();
+            
+            Dictionary<char, int> charCountDictionary  = new Dictionary<char, int>();
+
+            for(int i = 0; i < content.Length;i++){
+                if(charCountDictionary.ContainsKey(content[i])) charCountDictionary[content[i]]++;
+                else charCountDictionary.Add(content[i], 1);
+            }
+
+            foreach(var keyValuePair in charCountDictionary){
+                Console.WriteLine(keyValuePair.Key + ": " + keyValuePair.Value);
+            }
+
         } catch (Exception ex)
         {
             Console.WriteLine("Failed to read file: " + filePath + "\nError: " + ex.Message);
         }
-
-
     }
 }
