1f184ad080aed07167ae389448e775987a60a62f
KevJSDevelopment <Kshhn12@gmail.com>
2024-07-12 18:58:13 -0400

    step 1 completed

--- CompressionTest/obj/Debug/net8.0/CompressionTest.AssemblyInfo.cs
+++ CompressionTest/obj/Debug/net8.0/CompressionTest.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CompressionTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e6d9b634a08a206394216ca3860fbb113a353b1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0db343c576f9f15ac0dcc2075999912fd9475905")]
 [assembly: System.Reflection.AssemblyProductAttribute("CompressionTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CompressionTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- compression_tool/Program.cs
+++ compression_tool/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        if (args.Length <= 0) {
+            Console.WriteLine("Please specify a file name");
+            return;
+        }
+
+        string filePath = args[0];
+
+        try {
+            using StreamReader streamReader = new StreamReader(filePath);
+
+            string content = streamReader.ReadToEnd();
+        } catch (Exception ex)
+        {
+            Console.WriteLine("Failed to read file: " + filePath + "\nError: " + ex.Message);
+        }
+
+
+    }
+}
